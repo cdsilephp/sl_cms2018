9449083b41cfb0f93fb98210426a472c1b2b28a1
JunShiFighting <315133250@qq.com>
2018-12-15 14:58:44 +0800

    修改scope参数

--- silephp/libraries/WxGetUser.class.php
+++ silephp/libraries/WxGetUser.class.php
@@ -31,7 +31,7 @@ class WxGetUser{
     */
     private function get_code($callback){
         $appid = $GLOBALS['config_account']['WX_APPID'];
-        $scope = 'sile_userinfo';
+        $scope = 'sile';
         $state = md5(uniqid(rand(), TRUE));//唯一ID标识符
         $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' . $appid . '&redirect_uri=' . urlencode($callback) .  '&response_type=code&scope=' . $scope . '&state=' . $state . '#wechat_redirect';
         header("Location:$url");
