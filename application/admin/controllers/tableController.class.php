d10624f1a5c43e36bf486d13b601b1fa95e44730
wahson-gong@qq.com <582797956@qq.com>
2019-01-22 17:46:26 +0800

    新增通用访问接口等总开关

--- application/admin/controllers/tableController.class.php
+++ application/admin/controllers/tableController.class.php
@@ -122,6 +122,7 @@ class tableController extends baseController{
 	    //1.收集表单数据
 	    $data=$tableModel->getFieldArray();
 	    $data['u4'] = empty($data['u4'])?"off":"on";
+	    $data['u11'] = empty($data['u11'])?"off":"on";
 	    $data['u1']=$GLOBALS['config_db']['prefix'].$data['u1'];
 	    $data['u10'] = $data['u10']=="" ?0:$data['u10'];
 	    
@@ -160,6 +161,7 @@ class tableController extends baseController{
 	    //1.收集表单数据
 	    $data=$tableModel->getFieldArray();
 	    $data['u4'] = empty($data['u4'])?"off":"on";
+	    $data['u11'] = empty($data['u11'])?"off":"on";
 	    $data['u10'] = $data['u10']=="" ?0:$data['u10'];
 	    
 	   
