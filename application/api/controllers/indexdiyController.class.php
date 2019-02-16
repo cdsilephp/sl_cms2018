ba0cfd6ded561de1f3376f74e5fd6ab528caecfe
wahson-gong@qq.com <582797956@qq.com>
2019-02-16 17:16:03 +0800

    ghy

---
+++ application/api/controllers/indexdiyController.class.php
@@ -0,0 +1,29 @@
+<?php
+/**
+
+* 自定义接口文件
+
+* @date: 2019年2月14日 下午2:07:17
+
+* @author: 龚华尧
+
+* @param: $GLOBALS
+
+* @return:
+
+*/
+
+
+
+class indexdiyController extends baseController {
+     
+    //构造方法
+    public function __construct(){
+       $this->common = new Common();
+    }
+    
+    
+     
+	 
+	
+}
